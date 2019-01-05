427bf04cd115e2869052ea60c54890caab7545d6
Jonas Van Hoof <36266973+JonasVanHoof@users.noreply.github.com>
2019-01-05 14:00:54 +0100

    MicReader class added

--- app/src/main/java/com/example/michiel/soundmeter/MainActivity.java
+++ app/src/main/java/com/example/michiel/soundmeter/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.michiel.soundmeter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -16,11 +17,13 @@ TextView TmaxScore;
 ProgressBar progressBar;
 int scoreValue;
 int maxScoreValue;
+MicReader micReader;
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        micReader = new MicReader();
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
         Tscore = (TextView)findViewById(R.id.Tscore);
         TmaxScore = (TextView)findViewById(R.id.TmaxScore);
@@ -29,11 +32,15 @@ int maxScoreValue;
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
-                    scoreValue = progressBar.getProgress();
+                    micReader.onResume();
+                    scoreValue = (int)micReader.lastLevel;
                     Tscore.setText(Integer.toString(scoreValue));
-                    setMaxScoreValue();
+                    Log.i("micReader",Integer.toString(scoreValue));
+                    /*scoreValue = progressBar.getProgress();
+                    Tscore.setText(Integer.toString(scoreValue));
+                    setMaxScoreValue();*/
                 }else{
-
+                    micReader.onPause();
                 }
             }
         });
