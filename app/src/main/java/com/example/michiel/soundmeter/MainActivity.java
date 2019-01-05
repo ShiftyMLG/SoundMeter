d60be79990e83d73704da269d7221578fd5d8b84
Jonas Van Hoof <36266973+JonasVanHoof@users.noreply.github.com>
2019-01-05 14:45:07 +0100

    Progress value is recorded value

--- app/src/main/java/com/example/michiel/soundmeter/MainActivity.java
+++ app/src/main/java/com/example/michiel/soundmeter/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.michiel.soundmeter;
 
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CompoundButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -16,13 +20,19 @@ TextView TmaxScore;
 ProgressBar progressBar;
 int scoreValue;
 int maxScoreValue;
-MicReader micReader;
+
+private static final int sampleRate = 8000;
+private AudioRecord audio;
+private int bufferSize;
+private double lastLevel = 0;
+private Thread thread;
+private static final int SAMPLE_DELAY = 200;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        micReader = new MicReader();
         progressBar = (ProgressBar)findViewById(R.id.progressBar);
         Tscore = (TextView)findViewById(R.id.Tscore);
         TmaxScore = (TextView)findViewById(R.id.TmaxScore);
@@ -31,14 +41,19 @@ MicReader micReader;
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
-                    scoreValue = progressBar.getProgress();
-                    Tscore.setText(Integer.toString(scoreValue));
-                    setMaxScoreValue();
+                    onResume();
                 }else{
-
+                    onPause();
                 }
             }
         });
+        try {
+            bufferSize = AudioRecord
+                    .getMinBufferSize(sampleRate, AudioFormat.CHANNEL_IN_MONO,
+                            AudioFormat.ENCODING_PCM_16BIT);
+        } catch (Exception e) {
+            android.util.Log.e("TrackingFlow", "Exception", e);
+        }
     }
 
     private void setMaxScoreValue(){
@@ -46,5 +61,95 @@ MicReader micReader;
             maxScoreValue = scoreValue;
             TmaxScore.setText(Integer.toString(maxScoreValue));
         }
+    }
+
+    protected void onResume() {
+        super.onResume();
+        audio = new AudioRecord(MediaRecorder.AudioSource.MIC, sampleRate,
+                AudioFormat.CHANNEL_IN_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, bufferSize);
+        Log.i("micReader", "in onResume()");
+        audio.startRecording();
+        Log.i("micReader", "audio.startRecording()");
+        thread = new Thread(new Runnable() {
+            public void run() {
+                while(thread != null && !thread.isInterrupted()){
+                    //Let's make the thread sleep for a the approximate sampling time
+                    try{Thread.sleep(SAMPLE_DELAY);}catch(InterruptedException ie){ie.printStackTrace();}
+                    readAudioBuffer();//After this call we can get the last value assigned to the lastLevel variable
+
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            if(lastLevel > 0 && lastLevel <= 50){
+                                //toast message?
+                                Log.i("micReader","low value");
+                            }else
+                            if(lastLevel > 50 && lastLevel <= 100){
+                                //toast message?
+                                Log.i("micReader","medium value");
+                            }else
+                            if(lastLevel > 100 && lastLevel <= 170){
+                                //toast message?
+                                Log.i("micReader","high value");
+                            }
+                            if(lastLevel > 170){
+                                //toast message?
+                                Log.i("micReader","insane value");
+                            }
+                        }
+                    });
+                }
+            }
+        });
+        thread.start();
+        Log.i("micReader","thread.start");
+    }
+
+    /**
+     * Functionality that gets the sound level out of the sample
+     */
+    private void readAudioBuffer() {
+
+        try {
+            short[] buffer = new short[bufferSize];
+
+            int bufferReadResult = 1;
+
+            if (audio != null) {
+
+                // Sense the voice...
+                bufferReadResult = audio.read(buffer, 0, bufferSize);
+                double sumLevel = 0;
+                for (int i = 0; i < bufferReadResult; i++) {
+                    sumLevel += buffer[i];
+                }
+                lastLevel = Math.abs((sumLevel / bufferReadResult));
+                scoreValue = (int)lastLevel;
+                Tscore.setText(Integer.toString(scoreValue));
+                progressBar.setProgress(scoreValue);
+
+                Log.i("micReader",Integer.toString(scoreValue));
+                setMaxScoreValue();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        thread.interrupt();
+        thread = null;
+        try {
+            if (audio != null) {
+                audio.stop();
+                audio.release();
+                audio = null;
+            }
+        } catch (Exception e) {e.printStackTrace();}
     }
 }
